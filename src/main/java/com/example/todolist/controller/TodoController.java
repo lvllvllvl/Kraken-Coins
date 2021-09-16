c4ce4f6c5caecb1cc61aa1db216245fdd681a3e7
mohammad <lvl.azimi@gmail.com>
2021-09-16 19:23:16 +0430

    start of the project

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,43 @@
+package com.example.todolist.controller;
+
+
+import com.example.todolist.model.TodoItem;
+import com.example.todolist.repo.TodoRepo;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/todo")
+
+public class TodoController {
+
+    @Autowired
+
+    private TodoRepo todoRepo;
+
+    @GetMapping
+    public List<TodoItem> findAll() {
+        return todoRepo.findAll();
+    }
+
+    @PostMapping
+    public TodoItem save(@NotNull @RequestBody TodoItem todoItem) {
+
+        return todoRepo.save(todoItem);
+    }
+
+    @PutMapping
+    public TodoItem update(@NotNull @RequestBody TodoItem todoItem) {
+
+        return todoRepo.save(todoItem);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id) {
+        todoRepo.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/todolist/model/TodoItem.java
@@ -0,0 +1,53 @@
+package com.example.todolist.model;
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TodoItem {
+
+
+    private Long id;
+    @NotNull
+    private String title;
+    private boolean done;
+
+    public TodoItem() {
+    }
+
+    public TodoItem(Long id, String title, Boolean done) {
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
